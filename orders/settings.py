a73ec049ebe0c4a1ccdcddf4ab67095ca3b47dbe
Andrey Rahimov <rahimov.work@gmail.com>
2023-10-07 15:15:57 +0600

    feat: add throttling

--- orders/settings.py
+++ orders/settings.py
@@ -156,6 +156,16 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle'
+    ],
+
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '15/minute',
+        'anon': '5/minute',
+    },
 }
 
 # Celery settings
